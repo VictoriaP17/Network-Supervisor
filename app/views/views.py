ba895abc3d73108e83c1edb42fcd16a28d763e22
Victoria <ricardoperezayon9@gmail.com>
2025-03-30 14:41:35 -0600

    Created new pages, added placeholder values

--- app/views/views.py
+++ app/views/views.py
@@ -7,3 +7,13 @@ main_views = Blueprint("main_views", __name__)
 @main_views.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
+
+
+@main_views.route("/discovery", methods=["GET"])
+def discovery_index():
+    return render_template("discovery.html")
+
+
+@main_views.route("/control_center/<device_id>", methods=["GET"])
+def control_center_index(device_id):
+    return render_template("device_console.html")
